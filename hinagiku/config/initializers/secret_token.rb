0327a7b6268d88a79c0e0a8869507fc0b6266d44
Takayoshi Kohayakawa <ktaka.user@gmial.com>
2011-03-20 22:04:54 +0900

    add hinagiku study of Rails3

---
+++ hinagiku/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hinagiku::Application.config.secret_token = '4af383f5bcbba1636d22990a33ee2836b778d41353a558f70b688d236ef6438280b31babe537547cfb5c386af29f57c9481133be195c6264f4cebca490de710f'
